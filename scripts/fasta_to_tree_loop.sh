999ed361258b006f27b82cd367b71ebe886dd19f
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 11:39:22 +0100

    Update fasta_to_tree_loop.sh
    Annotation

--- scripts/fasta_to_tree_loop.sh
+++ scripts/fasta_to_tree_loop.sh
@@ -1,7 +1,8 @@
+#[DEPRECATED]
 #iterates down mouse-strains-list (which are the directory folders also) and runs fasta_to_tree on each.
-#! EDIT FASTA_TO_TREE BEFORE RUNNING LOOP !
 #if doing sequence alignment: conda activate mafft_conda
 #if doing tree generation: conda activate R
+#!/bin/bash
 cd ~/rds/rds-acf1004-afs-lab-rds/genomes/Mouse-strains
 
 while read name
