9240ae913cd6b311baead1158e3f7495f9a196c6
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 11:52:00 +0100

    Update name_adder_for_mafft.R
    Annotation

--- scripts/name_adder_for_mafft.R
+++ scripts/name_adder_for_mafft.R
@@ -2,7 +2,8 @@ suppressMessages(require(tidyverse))
 suppressMessages(require(data.table))
 suppressMessages(require(stringr))
 
-name = commandArgs(trailingOnly=TRUE)
+#pass in name (not actually used for anything anymore, but scripts that call the script all have it this way. It's kept for convenience)
+name = commandArgs(trailingOnly=TRUE) 
 if (length(name)==0 | length(name)>1) {
   stop("One argument must be supplied (input file).n", call.=FALSE)
 }
@@ -11,9 +12,9 @@ if (length(name)==0 | length(name)>1) {
 temp = fread("in-file.txt", fill=TRUE)
 name = str_sub(temp$V4, 4, -3) # Prefix for LTR names
 
-temp = temp %>% mutate(name = paste0(name,row_number())) %>% 
-  mutate(name2 = name) %>%
-  filter(as.numeric(V3) - as.numeric(V2) > 200)
+temp = temp %>% mutate(name = paste0(name,row_number())) %>% #add row number to name so each has a unique ID
+  mutate(name2 = name) %>% #duplicate name for bedtools getfasta
+  filter(as.numeric(V3) - as.numeric(V2) > 200) #filter out any LTRs less than 200bp (are likely fragments and not of interest)
 
 write.table(temp[,c(1,2,3,7,8,6)], file="outfile.txt", quote=FALSE, 
-            row.names=FALSE, col.names=FALSE, sep="\t")
+            row.names=FALSE, col.names=FALSE, sep="\t") #saving with the new names
