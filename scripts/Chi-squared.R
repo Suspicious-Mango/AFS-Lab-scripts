6b0663e594188a1287b6020012d56b83bdef8411
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 11:08:53 +0100

    Update Chi-squared.R
    Annotation

--- scripts/Chi-squared.R
+++ scripts/Chi-squared.R
@@ -1,24 +1,22 @@
 suppressMessages(require(dplyr))
 suppressMessages(require(data.table))
 
-#ss-counts.txt ss-control-counts.txt shared-counts.txt shared-control-counts.txt VM-counts.txt VM-control-counts.txt non-VM-counts.txt non-VM-control-counts.txt
-
 #in-matrix.txt example
-#SS SS-control Shared Shared-control VM VM-control non-VM non-VM-control
-#9371	1389	14451	14451	13694	13888	5232	9219
-#4252	10045	0	0	13	0	1272	13179
+#			SS		SS-control Shared 	Shared-control   VM 	VM-control non-VM non-VM-control
+#not found	9371	1400		14451	14451			14154	14363		5232	1400
+#    found	5080	13051		0		0				297		88			9219	13051
 
-data = read.table("in-matrix.txt", header = TRUE)
+data = read.table("in-matrix.txt", header = TRUE) #generated from DEG-enrichment.sh
 
-chi_sqr_ss = chisq.test(data[,1:2])
-chi_sqr_shared = chisq.test(data[,3:4])
-chi_sqr_VM = chisq.test(data[,5:6])
-chi_sqr_nonVM = chisq.test(data[,7:8])
+#run chi squared significance tests for each category vs it's control
+chi_sqr_ss = chisq.test(data[,1:2]) #SS vs SS-control
+chi_sqr_shared = chisq.test(data[,3:4]) #shared vs shared control
+chi_sqr_VM = chisq.test(data[,5:6]) #VM vs VM-control
+chi_sqr_nonVM = chisq.test(data[,7:8]) #non-VM vs non-VM control
 
-
+#save pvalues in a 1 X 4 matrix
 chi_sqr_table = as.table(matrix(c(chi_sqr_ss$p.value, chi_sqr_shared$p.value, chi_sqr_VM$p.value, chi_sqr_nonVM$p.value),
 			       	nrow = 1, ncol = 4))
 
-
 write.table(chi_sqr_table, file="outfile.txt", quote=FALSE, 
-            row.names=FALSE, col.names=FALSE, sep="\t")
+            row.names=FALSE, col.names=FALSE, sep="\t") #save
