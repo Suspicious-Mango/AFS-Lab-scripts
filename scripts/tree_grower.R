d15fdbb12f27eb3775459a52dbc8efff8a12d643
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 13:06:22 +0100

    Update tree_grower.R
    Annotation

--- scripts/tree_grower.R
+++ scripts/tree_grower.R
@@ -8,24 +8,25 @@ suppressMessages(require(reshape))
 suppressMessages(require(tidyr))
 suppressMessages(require(ggplotify))
 
-name = commandArgs(trailingOnly=TRUE)
+name = commandArgs(trailingOnly=TRUE) #read in the name for the plot title
 if (length(name)==0 | length(name)>1) {
   stop("One argument must be supplied (input file).n", call.=FALSE)
-}
+} 
 
-treefile = read.newick("in-tree.txt") 
+treefile = read.newick("in-tree.txt") #phylip_tree
 
 #fastafile = "in-fasta.txt"
 
+#color code
 NT_colors = c("a" = "chartreuse2", "t" = "coral2", "c" = "deepskyblue1", "g" = "darkgoldenrod2", "-" = "white")
 
-treeplot = treefile %>% ggtree() +
+treeplot = treefile %>% ggtree() + #generate tree
 	theme_tree2() +
 	labs(title = name, 
 		 subtitle = "IAPLTR1 clustering and sequence alignment")
 
-#duoplot = msaplot(treeplot, fastafile, bg_line = FALSE, color = NT_colors) +
+#duoplot = msaplot(treeplot, fastafile, bg_line = FALSE, color = NT_colors) + #generate msa + tree plot
 #	labs(fill = "Sequence") #+ geom_tiplab()	
 
 
-ggsave("outfile.png", treeplot, device = "png", height = 8, width = 12)
+ggsave("outfile.png", treeplot, device = "png", height = 8, width = 12) #saving
