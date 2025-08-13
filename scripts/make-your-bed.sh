5665c4cfc6b6b4cbad4e8613476a61d6a11e5050
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 11:42:38 +0100

    Update make-your-bed.sh
    Annotation

--- scripts/make-your-bed.sh
+++ scripts/make-your-bed.sh
@@ -1,2 +1,8 @@
-for file in ./*.txt ; do  name=`basename $file .txt` ;  cut -f5,6,7,10,15,9 $file | sed 's/C/-/g' > $name.bed  ; done
-
+#!/bin/bash
+#Example of how to convert a fasta.out into a bed6
+for file in ./*.txt
+do  
+  name=`basename $file .txt`
+  cut -f5,6,7,10,15,9 $file | 
+    sed 's/C/-/g' > $name.bed
+done
