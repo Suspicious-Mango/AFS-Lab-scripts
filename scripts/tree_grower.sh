c3e486f992ff8730d3d39e6cfdfc40b5cdac07b2
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 13:08:03 +0100

    Update tree_grower.sh
    Annotation

--- scripts/tree_grower.sh
+++ scripts/tree_grower.sh
@@ -1,17 +1,18 @@
-#conda activate R prior
-
+#!/bin/bash
 cd ~/rds/rds-acf1004-afs-lab-rds/genomes/Mouse-strains
 
-while read name
+while read name #run tree_grower.R for all strains
 do
-#run tree_grower.R for all strains
-cd $name
-echo -e "\nProcessing $name\n"
-cp $name.phylip_phyml_tree_$name.txt in-tree.txt
-#cp trimal-$name.fa in-fasta.txt
-conda activate -R Rscript ../scripts/tree_grower.R $name
-cp outfile.png LTR_phylogeny_$name.png
-rm in-tree.txt outfile.png #in-fasta.txt
-cd ../
-
+  cd $name
+  
+  echo -e "\nProcessing $name\n"
+  cp $name.phylip_phyml_tree_$name.txt in-tree.txt
+  cp trimal-$name.fa in-fasta.txt #copy files for tree_grower.R
+  
+  conda activate -R Rscript ../scripts/tree_grower.R $name 
+  
+  cp outfile.png LTR_phylogeny_$name.png #save outfile as unique name
+  
+  rm in-tree.txt outfile.png in-fasta.txt #cleanup
+  cd ../
 done < mouse-strains+hybrids-list.txt
