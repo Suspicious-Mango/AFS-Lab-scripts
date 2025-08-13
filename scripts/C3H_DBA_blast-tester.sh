6b8f92a543ad7d4ae497e39147629c1c53baa416
Suspicious-Mango <barrnicolas95@gmail.com>
2025-08-13 11:03:52 +0100

    Update C3H_DBA_blast-tester.sh
    Annotation

--- scripts/C3H_DBA_blast-tester.sh
+++ scripts/C3H_DBA_blast-tester.sh
@@ -1,6 +1,3 @@
-
+#test script for blastn using C3H and DBA. Didn't work as well as hoped, so we moved to using liftOver
 
 blastn -task blastn -query ~/rds/rds-acf1004-afs-lab-rds/genomes/Mouse-strains/C3H_HeJ/IAPLTR1-flanks-C3H_HeJ.fa -db ~/rds/rds-acf1004-afs-lab-rds/genomes/Mouse-strains/DBA_2J/DBA_2J_chromosomes_MT_unplaced.fasta -outfmt 6 > blast-C3H_HeJ-DBA_2J.txt
-
-
-
